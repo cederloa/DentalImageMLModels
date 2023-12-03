2d04989d0326898fe14beecb536e6a775bb7318e
Antti Cederlöf <antti.cederlof@tuni.fi>
2023-12-03 17:14:08 +0200

    Initializing main

---
+++ main.py
@@ -0,0 +1,16 @@
+# Author: Antti Cederlöf
+#
+# Dataset: Panoramic Dental X-rays With Segmented Mandibles.
+# A. H. Abdi, S. Kasaei, and M. Mehdizadeh, “Automatic segmentation of
+# mandible in panoramic x-ray,” J. Med. Imaging, vol. 2, no. 4, p. 44003, 2015.
+
+def read_data():
+    return
+
+
+def main():
+    read_data()
+
+
+if __name__ == '__main__':
+    main()
